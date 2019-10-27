c6eb36264834afc9cee415c71d76e020c097fc12
Markersa <Markersa@163.com>
2019-10-27 15:41:13 +0800

    tmx地图加载完成 并修改服务器的对数据数组的渲染方式

--- BlockChocolate.js
+++ BlockChocolate.js
@@ -4,7 +4,7 @@
 var TitleWalkMax = 3;
 var Timer = 0;
 var TimerMax = 60;
-var FPS = 240;
+var FPS = 144;
 
 //引擎
 function BlockChocolateEngine(Canvas,width,height){
@@ -44,7 +44,6 @@ function BlockChocolateEngine(Canvas,width,height){
         if(MainCamera==null||MainCamera ==undefined){
             console.log('[BC Engine Ver 1.0 ] Error Message: 引擎至少需要一个Camera  作为 主视角！');
         }else{
-            
         ThreadId = setInterval( function(){
             mCanvas.getContext('2d').fillStyle="#FFFFFF";
             mCanvas.getContext('2d').fillRect(0,0,mCanvas.width,mCanvas.height);
@@ -52,15 +51,18 @@ function BlockChocolateEngine(Canvas,width,height){
             if(TitleMapManger!=undefined){
                 for(var index= 0 ; index <TitleMapManger.getLayers().length;index++){
                     if(TitleMapManger.getLayers()[index].IsMapBG){
-                        
                     TitleMapManger.getLayers()[index].SetCanvas(mCanvas);
                     TitleMapManger.getLayers()[index].DrawOnLayer();
                     }
                 }
+                if(TitleMapManger.GetCollLayer()!=null||TitleMapManger.GetCollLayer()!=undefined){
                 TitleMapManger.GetCollLayer().SetCanvas(mCanvas);
                 TitleMapManger.GetCollLayer().DrawOnLayer();
+                }
+                if(TitleMapManger.GetInteractiveLayer()!=null||TitleMapManger.GetInteractiveLayer()!=undefined){
                 TitleMapManger.GetInteractiveLayer().SetCanvas(mCanvas);
                 TitleMapManger.GetInteractiveLayer().DrawOnLayer();
+                }
                 
                 for(var index= 0 ; index <TitleMapManger.getLayers().length;index++){
                     if(!TitleMapManger.getLayers()[index].IsMapBG){
@@ -80,9 +82,11 @@ function BlockChocolateEngine(Canvas,width,height){
         clearInterval(ThreadId);
     }
 }
+var DebugOnce = true;
 //图层
-function Layer(IsMapBG,bc){
+function Layer(IsMapBG,bc,TITLE){
     this.mcanvas;
+    this.Title = TITLE;
     var  Bc = bc;
     this.IsMapBG = IsMapBG;
     //设置是否为背景地图图层
@@ -100,16 +104,10 @@ function Layer(IsMapBG,bc){
             this.Sprites[index].DrawSelf(this.mcanvas); 
         }
     }else{
-            for(var sprite_index  = 0 ; sprite_index<this.Sprites.length;sprite_index++){
-                for(var sprite = 0 ; sprite<this.Sprites[sprite_index].length;sprite++){
-                    var TITLE = Bc.GetTitleMapMange().GetTitle(this.Sprites[sprite_index][sprite]);
-                    if(TITLE!=null){
-               DrawMap(Bc,[sprite,sprite_index],this.mcanvas,TITLE.TITLEIMAGE);
-                    }
-                }
-            
+            for(var sprite_index  = 2 ; sprite_index<this.Sprites.length;sprite_index++){
+               DrawMap(Bc,[parseInt((sprite_index-2)%this.Sprites[1][0]),parseInt((sprite_index-2)/this.Sprites[1][0])],this.mcanvas,this.Title,this.Sprites[sprite_index]);
             }
-    }
+            }
         mcanvas = undefined;
     }
     //图层所 包含 的所有精灵 集合
@@ -132,6 +130,7 @@ function Layer(IsMapBG,bc){
     }
     //返回该图层中的精灵集合
     this.getSprites = function(){
+        
         return this.Sprites;
     }
     //为该 图层 精灵集合 设置 数据源  被设置图层可以是 背景图层 或者 精灵图层
@@ -144,7 +143,6 @@ function Sprite(bc,XY,Name,data){
     var IsColl;
     this.Name = Name;
     this.data =data ;
-    this.Animes;
     this.Direction = [0,-1];
     this.PIc ;
     var  Bc = bc ;
@@ -152,6 +150,7 @@ function Sprite(bc,XY,Name,data){
     this.TitleY=XY[1];
     this.AnimeX= 0;
     this.AnimeY = 0; 
+    this.Animes;
     this.Layer ;
     this.Target = undefined;
     //this.RealMove = false;
@@ -161,9 +160,11 @@ function Sprite(bc,XY,Name,data){
     this.SetAnime = function(Anime){
         if(this.target==undefined){
         this.CurrentAnime = Anime;
+        this.Animes.AnimeStep = 0;
+        console.log("重新播放动画");
+        console.log("Anime:"+this.CurrentAnime);
         if(this.CurrentAnime !=Anime&&this.CurrentAnime!=undefined){
-            
-        this.CurrentAnime.AnimeStep = 0;
+        this.Animes.AnimeStep = 0;
         }
         }
         
@@ -175,8 +176,8 @@ function Sprite(bc,XY,Name,data){
     //播放 当前精灵的  当前动画
     this.PlayAnime= function(target){
             
-         this.AnimeX= this.CurrentAnime.AnimeStep/(TimerMax*target[0]);
-         this.AnimeY= this.CurrentAnime.AnimeStep/(TimerMax*target[1]);
+         this.AnimeX= parseFloat(this.Animes.AnimeStep/(TimerMax*target[0]));
+         this.AnimeY= this.Animes.AnimeStep/(TimerMax*target[1]);
          if(target[0]==0){
              this.AnimeX = 0;
          }
@@ -184,7 +185,7 @@ function Sprite(bc,XY,Name,data){
              this.AnimeY = 0;
          }
         
-         return this.CurrentAnime.AnimeRun();
+         return this.Animes.AnimeRun();
     }
     //*****重要方法*******
     //*****重要方法*******
@@ -196,23 +197,26 @@ function Sprite(bc,XY,Name,data){
     //对精灵 主动移动做出判断 并修改其AnimeX 和 AnimeY 在播放完其CurrentAnime 时 
     //会对 精灵的  TitleX TitleY 进行修改  完成一次完整的主动移动！
     //碰撞 检测也在 该方法内
-    this.Move2Target = function(Target,Anime){
+    this.Move2Target = function(Target,animedata){
         //检测 目标坐标是否为空(不为空 说明当前仍在移动 继续执行移动操作会导致不可预测的错误)
-        if(this.Target ==undefined){
+        if(this.Target ==undefined&&animedata!=undefined){
+            console.log("GETDATA:",Target,animedata);
             // 检测方法 检测 对象的目标坐标是否为障碍物;
           if(Bc.GetTitleMapMange().CollEvnetCheck([this.TitleX+Target[0],this.TitleY+Target[1]])){
               //如果目标坐标不是障碍物 则将坐标设置为该精灵的移动目标坐标
               this.Target = Target;
               //并将该精灵的当前动画设置为 参数动画
-              this.SetAnime(Anime);
+              this.SetAnime(animedata);
+              DebugOnce = false;
+              console.log("可以移动！："+animedata);
            }else{
                //检测 引擎是否添加 时间回调函数
             if(Bc.EventCallBack!=null||Bc.EventCallBack!=undefined){
                 //如果添加 则调用回调函数 并返回 flag 为 coll 的事件  包含 目标Sprite  以及 触发Sprite
                 this.Target = [0,0];
-                this.SetAnime(Anime);
-                Bc.EventCallBack(Event('Coll',Bc.GetTitleMapMange().GetCollLayer().getSprites()[this.TitleY+Target[1]][this.TitleX+Target[0]],this));
-            
+                this.SetAnime(animedata);
+                 Bc.EventCallBack(Event('Coll',Bc.GetTitleMapMange().GetCollLayer().getSprites()[Bc.GetTitleMapMange().GetCollLayer().getSprites()[1][0]*(this.TitleY+Target[1])+this.TitleX+2+Target[0]],this));
+                 console.log("Cant Move");
             }
            }
         }
@@ -241,27 +245,27 @@ function Sprite(bc,XY,Name,data){
     }
     //绘制 精灵自己的 PIc 图片才 Canvas 上
     this.DrawSelf = function(canvas){
-            if(this.Animes==null||this.Animes==undefined||this.Animes[0].imgs[0]==undefined||this.Animes[0].imgs[0]==null){
+            if(this.Animes==null||this.Animes==undefined){
             console.log('[BC Engine Ver 1.0 ] Error Message: 绘制精灵静态图片出现问题， 精灵需要至少一组动画！')
             }else{
                 if(this.PIc==null||this.PIc==undefined){
-                this.PIc = this.Animes[0].imgs[0];
+                this.PIc = [0,0];
                 }
                 if(this.CurrentAnime!=undefined){
-                    this.PIc = this.CurrentAnime.imgs[parseInt(this.CurrentAnime.AnimeStep/(TimerMax/ this.CurrentAnime.imgs.length))];
+                    this.PIc = [this.CurrentAnime,parseInt(this.Animes.AnimeStep/parseInt(TimerMax/Math.ceil(this.Animes.img.img.height/this.Animes.img.titleheight)))];
                     if(this.Target!=undefined){
                         if(this.PlayAnime(this.Target)){
                             if(this.Target[0]!=0||this.Target[1]!=0){
                                 this.Direction = this.Target;
                             }
-                            this.PIc =  this.CurrentAnime.imgs[0];
+                            this.PIc =  [this.CurrentAnime,0];
                             this.AnimeX = 0;
                             this.AnimeY = 0;
                             this.TitleX += this.Target[0];
                             this.TitleY += this.Target[1];
                             Bc.GetMainCamera().LeftTitleX+=this.Target[0];
                             Bc.GetMainCamera().TopTitleY+=this.Target[1];
-                            if(this.CurrentAnime.IsLoop){
+                            if(this.Animes.IsLoop){
                                 if(this.StopAnime){
                             this.CurrentAnime = undefined;
                             this.Target = undefined;
@@ -271,6 +275,8 @@ function Sprite(bc,XY,Name,data){
                                 this.CurrentAnime = undefined;
                                 this.Target = undefined;
                             }
+                            DebugOnce = true;
+                            console.log("动画完成");
                         }
                     }
                 }
@@ -283,11 +289,10 @@ function Sprite(bc,XY,Name,data){
         
     }
 }
-function BCAnime(imgs,IsLoop,Flag){
+function BCAnime(img,IsLoop){
     this.AnimeStep = 0;
-    this.Flag = Flag;
     this.IsLoop = IsLoop;
-    this.imgs = imgs;
+    this.img = img;
     this.AnimeRun = function(){
         if(this.AnimeStep<TimerMax){
             this.AnimeStep++;
@@ -325,9 +330,9 @@ function TitleMap(){
     this.AddNewTITLE = function(TITLE){
         Titles.push(TITLE);
     }
-    this.GetTitle = function(LOGO){
+    this.GetTitle = function(Flag){
         for(var index = 0; index<Titles.length;index++){
-            if(Titles[index].LOGO == LOGO){
+            if(Titles[index].Flag == Flag){
                 return Titles[index];
             }
         }
@@ -343,7 +348,8 @@ function TitleMap(){
         CollLayer = Layer;
     }
     this.CollEvnetCheck = function(TargetXy){
-        return CollLayer.getSprites()[TargetXy[1]][TargetXy[0]] =='space';
+         return CollLayer.getSprites()[CollLayer.getSprites()[1][0]*TargetXy[1]+TargetXy[0]+2][0]==-1&&
+        CollLayer.getSprites()[CollLayer.getSprites()[1][0]*TargetXy[1]+TargetXy[0]+2][1]==-1;
     }
     this.InteractiveEvnetCheck = function(TargetXy){
         var item = undefined;
@@ -364,47 +370,80 @@ function TitleMap(){
         return CollLayer;
     }
 }
-//瓦片
-function TITLE(LOGO,TITLEIMAGE){
-    this.LOGO = LOGO;
-    this.TITLEIMAGE = TITLEIMAGE;
-}
+
 //瓦片虚拟图片
-function TITLEIMAGE(image,left,top,width,height){
+function TITLEIMAGE(image,width,height,columns,Flag){
+    this.columns = parseInt(columns);
+    this.Flag = Flag;
 this.img = image;
-this.left= left;
-this.top = top;
-this.width = width;
-this.height = height;
+this.titlewidth = parseInt(width);
+this.titleheight = parseInt(height);
 }
 //数据工具类
 function THE_BEST_TOOL_CLASS(path){
     this.PATH = path;
     this.getImg=function(name){
         var image = new Image();
-        image.src = path+'/'+name;
+        image.src = this.PATH+'/'+name;
         return image;
     }
+    this.ReadXMLGetLayers = function(Name,TITLEMAPMANGER){ 
+        xmltext = new XMLHttpRequest;
+     xmltext.open("GET",this.PATH+"/"+Name,false);
+     xmltext.send();
+    tmxa = xmltext.responseXML;
+     tmx = tmxa.getElementsByTagName("map")[0].attributes;
+     tsxsrc = tmxa.getElementsByTagName('tileset');
+     tmxdata= tmxa.getElementsByTagName('layer');
+     xmltext.open("GET",this.PATH+"/"+tsxsrc[0].attributes.source.nodeValue,false);
+     xmltext.send();
+     tsxa = xmltext.responseXML;
+     tsxinfo = tsxa.getElementsByTagName('tileset');
+     tsxcolu = tsxinfo[0].attributes.columns.nodeValue;
+     twidth = tsxinfo[0].attributes.tilewidth.nodeValue;
+     theight = tsxinfo[0].attributes.tileheight.nodeValue;
+     tsxImg = tsxa.getElementsByTagName('image');
+     var Sharder = new TITLEIMAGE(this.getImg(tsxImg[0].attributes.source.nodeValue),twidth,theight,tsxcolu,tsxinfo[0].attributes.name.nodeValue);
+     for(var i = 0; i<tmxdata.length;i++){
+        if(i!=tmxdata.length-1){
+        var layer = tmxdata[i].textContent.trim().split(',');
+        var MapData = new Array();
+        MapData[0] = tsxinfo[0].attributes.name.nodeValue;
+        MapData[1] = [parseInt(tmxdata[i].attributes.width.nodeValue),parseInt(tmxdata[i].attributes.height.nodeValue)];
+        for(var index = 0 ; index < layer.length;index++){
+            MapData.push([parseInt(layer[index]-1)%Sharder.columns,Math.floor(parseInt(layer[index]-1)/Sharder.columns)]);
+        }
+    var Map = new Layer(true,BC,Sharder);
+    Map.SetMapData(MapData);
+    TITLEMAPMANGER.AddNewLayer(Map);
+    }else if(i==tmxdata.length-1){
+            var layer = tmxdata[i].textContent.trim().split(',');
+            var MapData = new Array();
+            MapData[0] = tsxinfo[0].attributes.name.nodeValue;
+            MapData[1] = [parseInt(tmxdata[i].attributes.width.nodeValue),parseInt(tmxdata[i].attributes.height.nodeValue)];
+            for(var index = 0 ; index < layer.length;index++){
+                MapData.push([parseInt(layer[index]-1)%Sharder.columns,Math.floor(parseInt(layer[index]-1)/Sharder.columns)]);
+            }
+        var Map = new Layer(true,BC,Sharder);
+        Map.SetMapData(MapData);
+        TITLEMAPMANGER.CollLayerAdd(Map);
+        }
+
+    }
+        
+   }
     
 }
 
-//    -4
-//    -3
-//    -2
-//    -1       
-// 0  -4 -0     -5 -1
-//     起始     向下移动
-/*
-公共绘图方法！
-*/
-function DrawMap(Camera,XY,canvas,img){
-    var ratio = Camera.TitleFormatHeight/img.height; 
+function DrawMap(Camera,XY,canvas,img,sprite){
+    var heightratio = Camera.TitleFormatHeight/img.titleheight; 
+    var widthration = Camera.TitleFormatWidth/img.titlewidth; 
     canvas.getContext("2d").drawImage(
         img.img,//原图片
-        img.left,//原图片截取位置的左上角X坐标
-        img.top,//原图片截取位置的左上角Y坐标
-        img.width,//原图片截取尺寸的宽
-        img.height,//原图片截取尺寸的高
+        (sprite[0])*img.titlewidth,//原图片截取位置的左上角X坐标
+        (sprite[1])*img.titleheight,//原图片截取位置的左上角Y坐标
+        img.titlewidth,//原图片截取尺寸的宽
+        img.titleheight,//原图片截取尺寸的高
         (XY[0]-Camera.GetMainCamera().getMainSprite().TitleX)*
         Camera.TitleFormatWidth-Camera.GetMainCamera().getMainSprite().AnimeX*Camera.TitleFormatWidth+
         ((Camera.TitileWidth-1)/2)*Camera.TitleFormatWidth,
@@ -417,39 +456,23 @@ function DrawMap(Camera,XY,canvas,img){
         //绘制的精灵在整体实际坐标系里的真实位置Y（按照Title瓦片计算）减去
           //摄像机在整体实际坐标系里的真是位置Y（按照Title瓦片计算） 取得当前需要绘制的精灵在摄像机里显示的位置（按照Title瓦片计算）
           // 乘上 摄像机 自动适配的瓦片大小 即可算得 绘制的精灵在摄像机里的真实位置（按照PX计算）
-        img.width*ratio+(Camera.TitleFormatWidth-img.width*ratio)/2,//将截取尺寸按比例缩小
-        img.height*ratio//将截取尺寸按比例缩小
+        img.titlewidth*widthration,//将截取尺寸按比例缩小
+        img.titleheight*heightratio//将截取尺寸按比例缩小
         );
     
     }
+    // 85.71428571428572 142.85714285714286 73 106 NaN 136.36363636363637 23.67679490321 27.27272727272727
 function DrawSprite(Camera,Sprite,canvas,img){
-    var ratio = Camera.TitleFormatHeight/img.height;
-    // if(Sprite.RealMove){
-    // canvas.getContext("2d").drawImage(
-    //     img.img,//原图片
-    //     img.left,//原图片截取位置的左上角X坐标
-    //     img.top,//原图片截取位置的左上角Y坐标
-    //     img.width,//原图片截取尺寸的宽
-    //     img.height,//原图片截取尺寸的高
-    //     (Sprite.TitleX-Camera.GetCamera().LeftTitleX)*Camera.TitleFormatWidth+Sprite.AnimeX*Camera.TitleFormatWidth+((Camera.TitileWidth-1)/2)*Camera.TitleFormatWidth,
-    //     //绘制的精灵在整体实际坐标系里的真实位置X（按照Title瓦片计算）减去
-    //      // 摄像机在整体实际坐标系里的真是位置X（按照Title瓦片计算） 取得当前需要绘制的精灵在摄像机里显示的位置（按照Title瓦片计算）
-    //      // 乘上 摄像机 自动适配的瓦片大小 即可算得 绘制的精灵在摄像机里的真实位置（按照PX计算）
-    //     (Sprite.TitleY - Camera.GetCamera().TopTitleY)*Camera.TitleFormatHeight + Sprite.AnimeY*Camera.TitleFormatHeight+((Camera.TitleHeight-1)/2)*Camera.TitleFormatHeight,
-    //     //绘制的精灵在整体实际坐标系里的真实位置Y（按照Title瓦片计算）减去
-    //       //摄像机在整体实际坐标系里的真是位置Y（按照Title瓦片计算） 取得当前需要绘制的精灵在摄像机里显示的位置（按照Title瓦片计算）
-    //       // 乘上 摄像机 自动适配的瓦片大小 即可算得 绘制的精灵在摄像机里的真实位置（按照PX计算）
-    //     img.width*ratio+(Camera.TitleFormatWidth-img.width*ratio)/2,//将截取尺寸按比例缩小
-    //     img.height*ratio//将截取尺寸按比例缩小
-    //     );
-    // }else{
+    var ratio = Camera.TitleFormatHeight/Sprite.Animes.img.titleheight;
+   
+    
       
         canvas.getContext("2d").drawImage(
-        img.img,//原图片
-        img.left,//原图片截取位置的左上角X坐标
-        img.top,//原图片截取位置的左上角Y坐标
-        img.width,//原图片截取尺寸的宽
-        img.height,//原图片截取尺寸的高
+            Sprite.Animes.img.img,//原图片
+        img[0]*Sprite.Animes.img.titlewidth,//原图片截取位置的左上角X坐标
+        img[1]*Sprite.Animes.img.titleheight,//原图片截取位置的左上角Y坐标
+        Sprite.Animes.img.titlewidth,//原图片截取尺寸的宽
+        Sprite.Animes.img.titleheight,//原图片截取尺寸的高
         (((Camera.TitileWidth-1)/2)+(Sprite.TitleX-Camera.GetMainCamera().getMainSprite().TitleX))*Camera.TitleFormatWidth-(
         Camera.GetMainCamera().getMainSprite().AnimeX-Sprite.AnimeX)*Camera.TitleFormatWidth,
         //绘制的精灵在整体实际坐标系里的真实位置X（按照Title瓦片计算）减去
@@ -461,8 +484,8 @@ function DrawSprite(Camera,Sprite,canvas,img){
         //绘制的精灵在整体实际坐标系里的真实位置Y（按照Title瓦片计算）减去
           //摄像机在整体实际坐标系里的真是位置Y（按照Title瓦片计算） 取得当前需要绘制的精灵在摄像机里显示的位置（按照Title瓦片计算）
           // 乘上 摄像机 自动适配的瓦片大小 即可算得 绘制的精灵在摄像机里的真实位置（按照PX计算）
-        img.width*ratio+(Camera.TitleFormatWidth-img.width*ratio)/2,//将截取尺寸按比例缩小
-        img.height*ratio//将截取尺寸按比例缩小
+          Sprite.Animes.img.titlewidth*ratio+(Camera.TitleFormatWidth-Sprite.Animes.img.titlewidth*ratio)/2,//将截取尺寸按比例缩小
+          Sprite.Animes.img.titleheight*ratio//将截取尺寸按比例缩小
         );
         
         var width = canvas.getContext('2d').measureText(Sprite.Name);
@@ -480,3 +503,5 @@ function DrawSprite(Camera,Sprite,canvas,img){
 function Event(flag,target,origin){
 return{flag:flag,target:target,origin:origin,behave:origin.CurrentAnime}
 }
+
+  
